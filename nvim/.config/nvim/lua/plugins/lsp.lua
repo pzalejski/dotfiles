d6fda4773bac0fa62dfa68d8cd0e4ad1261a02c1
pzalejski <163012189+pzalejski@users.noreply.github.com>
2025-07-12 13:09:00 -0400

    add: rust_analyzer and delay for inlay hints

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,3 +1,5 @@
+local set = vim.keymap.set
+
 return {
     {
         'mason-org/mason.nvim',
@@ -91,16 +93,22 @@ return {
                 callback = function(event)
                     local opts = { buffer = event.buf }
 
-                    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-                    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-                    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-                    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-                    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-                    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-                    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-                    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-                    vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-                    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+                    -- Enable inlay hints for all language servers
+                    vim.defer_fn(function()
+                        vim.lsp.inlay_hint.enable(true)
+                    end, 1000) -- some like rust_analyzer need a delay on first buffer
+
+                    set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+                    set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+                    set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+                    set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+                    set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+                    set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+                    set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+                    set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+                    set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+                    set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+                    set('n', '<leader>ih', '<cmd>lua vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())<cr>', opts)
                 end,
             })
 
@@ -121,6 +129,18 @@ return {
                                     diagnostics = {
                                         globals = { 'vim' }
                                     }
+                                }
+                            }
+                        })
+                    end,
+
+                    rust_analyzer = function()
+                        require('lspconfig').rust_analyzer.setup({
+                            settings = {
+                                ['rust-analyzer'] = {
+                                    diagnostics = {
+                                        enable = true
+                                    },
                                 }
                             }
                         })
