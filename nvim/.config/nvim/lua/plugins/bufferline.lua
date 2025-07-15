588d837d8028e04928f6cfc64e15fb460a065193
pzalejski <163012189+pzalejski@users.noreply.github.com>
2025-07-15 09:23:41 -0400

    feat: add lualine and bufferline

---
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,18 @@
+return {
+    'akinsho/bufferline.nvim',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+        local bufferline = require('bufferline')
+        bufferline.setup({
+            options = {
+                style_preset = bufferline.style_preset.minimal,
+                indicator = { style = 'none' },
+                numbers = 'buffer_id',
+                show_buffer_icons = false,
+                show_buffer_close_icons = false,
+                separator_style = 'slope'
+            }
+        })
+    end
+}
