d62658f081176262589f02be3d662c3de40cf08d
pzalejski <163012189+pzalejski@users.noreply.github.com>
2025-08-26 22:52:23 -0500

    feat: add transparency for floating/popup windows - telescope, mason, etc

--- nvim/.config/nvim/lua/config/ui.lua
+++ nvim/.config/nvim/lua/config/ui.lua
@@ -1,6 +1,9 @@
 -- Set both the Normal window and floating window background colors to transparent / none
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopeInsert", { bg = "none" })
+vim.api.nvim_set_hl(0, "cursorline", { bg = '#2a282f' })
 
 -- Non-Focused
-vim.api.nvim_set_hl(0, "NormalNC", { bg = "#191724" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
